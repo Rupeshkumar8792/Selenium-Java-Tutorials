b0a293d65a0c1ab8ea404b6c1e46870d91b95978
Rupeshkumar suryawanshi <suryawanshi.rupeshkumar8792@gmail.com>
2024-03-27 11:16:39 +0530

    launch browser

---
+++ src/test/java/SeleniumCodes/SeleniumPractiseCodes/launchBrowser/LaunchBrowser.java
@@ -0,0 +1,23 @@
+package SeleniumCodes.SeleniumPractiseCodes.launchBrowser;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchBrowser 
+{
+	@Test
+	public void launchBrowser()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));		
+	}
+}
