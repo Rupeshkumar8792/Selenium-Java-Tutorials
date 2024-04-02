540c2a9792cf9a1f5cfaaf6d74a3342bdff4f45b
Rupeshkumar suryawanshi <suryawanshi.rupeshkumar8792@gmail.com>
2024-04-02 21:23:29 +0530

    xpath using following,preceding,following-sibling and preceding-sibling

--- src/test/java/SeleniumCodes/SeleniumPractiseCodes/locators/advanceLocators/xPathUsingAxes/XpathUsingPrecedingSibling.java
+++ src/test/java/SeleniumCodes/SeleniumPractiseCodes/locators/advanceLocators/xPathUsingAxes/XpathUsingPrecedingSibling.java
@@ -10,18 +10,18 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class XpathUsingFollowing 
+public class XpathUsingPrecedingSibling 
 {
 	@Test
-	public void xpathUsingFollowing()
+	public void xpathUsingPrecedingSibling()
 	{
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-		driver.get("https://cosmocode.io/automation-practice-webtable/");
+		driver.get("https://selectorshub.com/xpath-practice-page/");
 		
-		WebElement input = driver.findElement(By.xpath("//td[text()='Kabul']//following::td"));
+		WebElement input = driver.findElement(By.xpath("//td[text()='Garry White']//preceding-sibling::td[text()='ESS']"));
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
@@ -30,4 +30,6 @@ public class XpathUsingFollowing
 		}
 		System.out.println(input.getText());
 	}
+	
+	
 }
